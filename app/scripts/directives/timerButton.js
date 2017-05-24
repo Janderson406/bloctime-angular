989260d4c9498b9aa228c0f22f397d0bd21def83
Jon Anderson <jondanderson@gmail.com>
2017-05-23 19:46:02 -0600

    Add addl 30 sec. timer and styling

--- app/scripts/directives/timerButton.js
+++ app/scripts/directives/timerButton.js
@@ -6,21 +6,22 @@
       //  replace: true,
       restrict: 'E',
       scope: {},
-      link: function(scope, element, attributes) {
-        var BREAK_TIME = 300; //will set up filter to convert seconds
-        var WORK_TIME = 1500;
+      link: function(scope, element, attributes) { //will set up filter to convert seconds
+        var BREAK_TIME = 3;         //300
+        var LONG_BREAK_TIME = 8; //1800
+        var WORK_TIME = 5;         //1500
 
         // INITIAL SETUP
         scope.buttonStatus = 'START';
         scope.currentTime = WORK_TIME;
         scope.onBreak = false;
         var countdown = undefined;
+        scope.numWorkSessions = 0;
 
         var startTimer = function() {
           scope.buttonStatus = 'RESET';
 
           countdown = $interval(function() {
-
             if (scope.currentTime >= 1) {
               scope.currentTime--;
             } else {
@@ -29,12 +30,12 @@
                 scope.currentTime = BREAK_TIME;
                 scope.buttonStatus = 'BREAK';
                 scope.onBreak = true;
+                scope.numWorkSessions++;
               } else if (scope.onBreak == true) {
                 scope.currentTime = WORK_TIME;
                 scope.buttonStatus = 'WORK';
                 scope.onBreak = false;
               }
-
               stopTimer();
               // alert("countdown end");
             }
@@ -49,7 +50,11 @@
             scope.buttonStatus = 'START';
             // alert("first stopTimer");
           } else if (scope.onBreak == true) { //BREAK RESET
-            scope.currentTime = BREAK_TIME;
+            if (scope.numWorkSessions % 4 === 0) {
+              scope.currentTime = LONG_BREAK_TIME;
+            } else {
+              scope.currentTime = BREAK_TIME;
+            };
             $interval.cancel(countdown);
             countdown = undefined;
             scope.buttonStatus = 'BREAK';
